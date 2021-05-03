1eae91c293a95f3ac435becdc079a330d8def41a
abirsaleh <khilgaon750@gmail.com>
2021-05-03 15:10:23 +0100

    exam1

---
+++ exam1/Proj1/Project2/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class Student:User
+    {
+        public int Fee { get; set; }
+
+        public Student()
+        {
+
+        }
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ exam1/Proj1/Project2/Teacher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class Teacher:User
+    {
+        public int Salary { get; set; }
+
+        public Teacher()
+        {
+               
+        }
+        public Teacher(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void Salary(int salary)
+        {
+            if (salary > 0)
+            {
+                
+            }           
+        }
+    }
+}
