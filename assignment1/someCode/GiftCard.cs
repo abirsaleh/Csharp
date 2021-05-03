aa9b3837706cea32bb5343d5fb958e5771208274
abirsaleh <khilgaon750@gmail.com>
2021-05-03 14:44:03 +0100

    1

---
+++ assignment1/someCode/GiftCard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace someCode
+{
+    class GiftCard:Item
+    {
+        public GiftCard():base(5.9)
+        {
+
+        }
+    }
+}
---
+++ assignment1/someCode/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace someCode
+{
+    class Product:Item
+    {
+        public Product():base(23.0)
+        {
+
+        }
+    }
+}
