5c4b8a49ce001a62e2abae5fa36bf218f113a80b
abirsaleh <khilgaon750@gmail.com>
2021-05-31 17:27:07 +0100

    case-10

---
+++ Case-10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Case_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PartialClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Case-10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
