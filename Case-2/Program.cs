63074bae5689e31ca944d506465c9076db24365c
abirsaleh <khilgaon750@gmail.com>
2021-05-31 17:21:52 +0100

    Case-11

---
+++ Case-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Case_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
