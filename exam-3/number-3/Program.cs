dbba10ec303cb83aa4474aa2616ac87af7a146be
abirsaleh <khilgaon750@gmail.com>
2021-06-21 16:43:01 +0100

    number 3

---
+++ exam-3/number-3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace number_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPrime = true;
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 2; i <= input; i++)
+            {
+                for (int j = 2; j <= input; j++)
+                {
+
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+
+                }
+                if (isPrime)
+                {
+                    Console.Write(", "+i);
+                }
+                isPrime = true;
+            }
+            Console.ReadKey();
+        }
+    }
+}
